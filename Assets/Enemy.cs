691146fe4b954959bbde4e82e70d7425ce9bb6df
LinSaki <saquika@sheridancollege.ca>
2025-10-21 15:27:24 -0400

    Updated Enemy timers and cooldowns

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,8 +2,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-   public void TakeDamage()
+    private SpriteRenderer sr;
+
+    [SerializeField] private float redColorDuration = 1;
+
+    public float timer;
+
+    private void Awake()
     {
-        Debug.Log(gameObject.name + " took damage!");
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer < 0 && sr.color != Color.white)
+            sr.color = Color.white; 
+
+    }
+
+    public void TakeDamage()
+    {
+        sr.color = Color.red;
+        timer = redColorDuration;
     }
 }
